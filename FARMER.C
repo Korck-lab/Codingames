24f4cea04f663d45573e82480fcfca7df6b5358f
korck <rafael.aguilher@gmail.com>
2022-01-15 21:51:57 -0300

    farmer done

--- FARMER.C
+++ FARMER.C
@@ -1,14 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
-
 #define byte unsigned char
 #define F (0x1 << 3)
 #define W (0x1 << 2)
 #define G (0x1 << 1)
 #define C (0x1 << 0)
-
 char *makeStr(char output[], byte input, bool inv)
 {
     char l = inv ? 'R' : 'L';
@@ -17,10 +14,8 @@ char *makeStr(char output[], byte input, bool inv)
     {
         output[p * 2] = (input & 0x1 << (3 - p)) ? r : l;
     }
-
     return output;
 }
-
 byte makeByte(char *input)
 {
     byte ret = 0;
@@ -36,7 +31,6 @@ byte makeByte(char *input)
     }
     return ret;
 }
-
 byte crossStep(byte state, bool invW)
 {
     bool Fa = (state & F); //1 0 0 0
@@ -44,96 +38,83 @@ byte crossStep(byte state, bool invW)
     bool Ga = (state & G); //1 0 1 0
     bool Ca = (state & C); //1 1 0 0
     bool mF = !Fa;         //always switch sides
-    bool mG = Fa == Ga ? (Wa < Ga && Ca < Ga) || (Wa > Ga && Ca > Ga) || (Wa == Ga && Ca == Ga): Ga;  
+    bool mG = Fa == Ga ? (Wa < Ga && Ca < Ga) || (Wa > Ga && Ca > Ga) || (Wa == Ga && Ca == Ga) : Ga;
     bool mWi = (Wa == Fa && !Wa && mG == Ga) || Wa;
     bool mCi = (Ca == Fa && !Ca && mWi == Wa && mG == Ga) || Ca;
     bool mC = (Ca == Fa && !Ca && mG == Ga) || Ca;
     bool mW = (Wa == Fa && !Wa && mC == Ca && mG == Ga) || Wa;
     byte nextState = (F * mF) | (W * mW) | (G * mG) | (C * mC);
     byte nextStatei = (F * mF) | (W * mWi) | (G * mG) | (C * mCi);
-    
-    return invW?nextStatei:nextState;
+    return invW ? nextStatei : nextState;
 }
 
-void tests()
+int solution(char output[][9], byte init, byte target)
 {
-    char ints[][9] = {"L L L L", "R R R R", "L R L R", "L R L L", "R L R R"};
-    byte tars[][9] = {"R L R L", "L R L R", "R R R R", "R R L R", "L L R R"};
-    char b[9]= "? ? ? ? ";
-    bool t = true;
-    bool f = false;
-    printf("%s - %s - %d\n",ints[0],makeStr(b, crossStep(makeByte(ints[0]), f), f), f);
-    printf("%s - %s - %d\n",ints[0],makeStr(b, crossStep(makeByte(ints[0]), t), t), t);
-    printf("%s - %s - %d\n",ints[1],makeStr(b, crossStep(makeByte(ints[1]), f), f), f);
-    printf("%s - %s - %d\n",ints[1],makeStr(b, crossStep(makeByte(ints[1]), t), t), t);
-    printf("%s - %s - %d\n",ints[2],makeStr(b, crossStep(makeByte(ints[2]), f), f), f);
-    printf("%s - %s - %d\n",ints[2],makeStr(b, crossStep(makeByte(ints[2]), t), t), t);
-    printf("%s - %s - %d\n",ints[3],makeStr(b, crossStep(makeByte(ints[3]), f), f), f);
-    printf("%s - %s - %d\n",ints[3],makeStr(b, crossStep(makeByte(ints[3]), t), t), t);
-    printf("%s - %s - %d\n",ints[4],makeStr(b, crossStep(makeByte(ints[4]), f), f), f);
-    printf("%s - %s - %d\n",ints[4],makeStr(b, crossStep(makeByte(ints[4]), t), t), t);
-}
-
-int solution(byte init, byte target)
-{
-    if(init == target)
+    if (init == target)
     {
-        char b[9];
+        char b[] = "? ? ? ?\n";
         printf(makeStr(b, init, false));
         return 0;
     }
-
-    bool inv = ((init&W)+(init&C)) > ((target&W)+(target&C));
-    if (inv) // XOR
-    {
-        init = init^0xF;
-        target = target^0xF;
-    }
+    byte iinit = init ^ 0xF;
+    byte itarget = target ^ 0xF;
     short maxP = 4 * 3 * 2 / (4 - 2);
     byte P0[maxP];
     byte P1[maxP];
-    P0[0] = init;
-    P1[0] = init;
+    byte P2[maxP];
+    byte P3[maxP];
+    P0[0] = P2[0] = iinit;
+    P1[0] = P3[0] = init;
     byte *P;
-    short c=0;
+    short c = 0;
+    bool inv = false;
     for (int i = 0; i < maxP - 1; i++)
     {
-        P0[i + 1] = crossStep(P0[i], false^inv);
-        P1[i + 1] = crossStep(P0[i], true^inv);
+        P0[i + 1] = crossStep(P0[i], true);
+        P1[i + 1] = crossStep(P1[i], false);
+        P2[i + 1] = crossStep(P2[i], false);
+        P3[i + 1] = crossStep(P3[i], true);
         c++;
-        if (P0[i+1] == target)
+        if (P0[i + 1] == itarget)
         {
-            P=P0;
+            P = P0;
             break;
         }
-        if (P1[i+1] == target)
+        if (P1[i + 1] == target)
         {
-            P=P1;
+            P = P1;
+            break;
+        }
+        if (P2[i + 1] == itarget)
+        {
+            P = P2;
+            inv = true;
+            break;
+        }
+        if (P3[i + 1] == target)
+        {
+            P = P3;
+            inv = true;
             break;
         }
     }
-    char output[] = "? ? ? ?\n";
-    for(int i=0; i<=c; i++)
+    char tmpo[] = "? ? ? ?\n";
+    for (int i = 0; i <= c; i++)
     {
-        printf(makeStr(output, P[i], inv));
+        makeStr(tmpo, P[i], P[0] == init ? false : true);
+        if (output == NULL)
+            printf("%s", tmpo);
+        if (output != NULL)
+            sprintf(output[i], "%s", tmpo);
     }
     return 0;
 }
-
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 int main()
 {
-    tests();
-
     char INITIAL[11];
     scanf("%[^\n]", INITIAL);
     fgetc(stdin);
     char TARGET[11];
     scanf("%[^\n]", TARGET);
-    byte init = makeByte(INITIAL);
-    byte target = makeByte(TARGET);
-    return(solution(init, target));
+    return (solution(NULL, makeByte(INITIAL), makeByte(TARGET)));
 }
